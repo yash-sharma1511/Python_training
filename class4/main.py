cab0266f0bb455866a034103174bd58b4bd07974
yash-sharma1511 <sharmayash151102@gmail.com>
2025-03-20 23:14:07 +0530

    main.py solution added

--- class4/main.py
+++ class4/main.py
@@ -1,1 +1,34 @@
-# 
+# Data Pipeline Validator
+def pipe(pipeline,threshold):
+    longest=max(pipeline,key=lambda x:x[1])[0]
+    exceed=[name for name,time in pipeline if time>threshold]
+    print("Longest Pipeline:",longest)
+    print("Pipelines exceeding threshold:", exceed)
+
+pipelines = [
+    ("Data Ingestion", 30),
+    ("Preprocessing", 45),
+    ("Model Training", 120),
+    ("Evaluation", 20)
+]
+threshold = 40
+
+pipe(pipelines,threshold)
+
+# Log File Parser
+def code(logs):
+    ans=set()
+    for line in logs.split('\n'):
+        if line.startswith('ERROR'):
+            parts=line.split()
+            if len(parts)>1:
+                ans.add(parts[1].replace(":",""))
+    return sorted(ans)            
+    
+logs = """ERROR 404: Not Found
+INFO: Connection established
+ERROR 500: Internal Server Error
+ERROR 404: Not Found
+"""
+
+print("Unique Error Code:",code(logs))
